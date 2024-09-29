5bb78f8c2c46ec94be8364b69ebaf14e58c6c4e0
Dominik Stasiak <d.stasiak436@gmail.com>
2024-09-29 17:04:20 +0200

    white pieces

---
+++ main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#define BOARD_SIZE 8
+
+char board[BOARD_SIZE][BOARD_SIZE];
+
+void initialize_board() {
+  for (int i = 0; i < BOARD_SIZE; i++) {
+    for (int j = 0; j < BOARD_SIZE; j++) {
+      board[i][j] = ' ';
+    }
+  }
+  // white - big leters
+  board[0][0] = board[0][7] = 'R'; // Rook
+  board[0][1] = board[0][6] = 'N'; // Knight
+  board[0][2] = board[0][5] = 'B'; // Bishop
+  board[0][3] = 'Q';               // Queen
+  board[0][4] = 'K';               // King
+  for (int i = 0; i < BOARD_SIZE; i++) {
+    board[1][i] = 'P'; // Pawns
+  }
+  printf("success");
+}
+
+int main() {
+  initialize_board();
+  return 0;
+}
